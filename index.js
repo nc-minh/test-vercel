50c43a7efc81b71e47a16e3f3678aea08903b1d0
nc-minh <nguyencongminh.official@gmail.com>
2022-02-27 17:20:30 +0700

    :memo: update vercel config

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const path = require("path");
 const app = express();
 const server = require("http").createServer(app);
 
+var cors = require('cors')
+app.use(cors())
+
 const io = require("socket.io")(server);
 
 app.use(express.static(path.join(__dirname + "/public")));
