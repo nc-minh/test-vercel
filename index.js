397ad3113ea4e3d09babd29662f7dec1f6f8a26c
nc-minh <nguyencongminh.official@gmail.com>
2022-05-08 13:28:25 +0700

    uo

--- index.js
+++ index.js
@@ -1,23 +1,11 @@
-const app = require("express")();
 const express = require("express");
-const http = require("http").Server(app);
-const io = require("socket.io")(http);
-const path = require("path");
-const port = process.env.PORT || 2512;
+const app = express();
+const port = 3000;
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+  res.send("Hello World!");
 });
 
-io.on("connection", (socket) => {
-  socket.on("chat message", (msg) => {
-    io.emit("chat message", msg);
-  });
-});
-
-//config public resources
-app.use("/public", express.static(path.join(__dirname, "/public")));
-
-http.listen(port, () => {
-  console.log(`Socket.IO server running at http://localhost:${port}/`);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
